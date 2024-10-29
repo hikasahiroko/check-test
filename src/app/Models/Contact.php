764aa4c3f989bf6455505bfacd08f3f1fdf177be
hikasa hiroko <k0t0_p1@yahoo.co.jp>
2024-10-29 19:46:44 +0900

    カテゴリテーブル更新

--- src/app/Models/Contact.php
+++ src/app/Models/Contact.php
@@ -9,14 +9,19 @@ class Contact extends Model
 {
     use HasFactory;
     protected $fillable = [
-         'first_name',
-         'last_name',
-         'gender',
-         'email',
-         'tel',
-         'address',
-         'building',
-         'select',
-         'content'
-     ];
+       'category_id',
+       'first_name',
+       'last_name',
+       'gender',
+       'email',
+       'tel',
+       'address',
+       'building',
+       'content'
+   ];
+
+    public function category()
+    {
+      return $this->belongsTo(Category::class);
+    }
 }
